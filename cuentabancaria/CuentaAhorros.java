43a0a09d4b73c186c7d669b060888dcba1025daf
AdrianaVelastegui <67924867+AdrianaVelastegui@users.noreply.github.com>
2020-08-09 23:21:46 -0500

    Add files via upload

---
+++ cuentabancaria/CuentaAhorros.java
@@ -0,0 +1,24 @@
+
+package cuentabancaria;
+
+/**
+ *
+ * @author Adriana
+ */
+public class CuentaAhorros extends CuentaBancaria {
+	
+	@Override
+	public void deposito(double dinero) {
+		
+		double totalDeposito = dinero < 1000 ? dinero + dinero *0.02: dinero;
+		this.saldo += totalDeposito;
+		//super.deposito(totalDeposito);
+		//Suma saldo total, super llama desde el método padre
+	
+		}
+	
+	public void imprimirSaldo() {
+		System.out.println("Saldo: "+ getSaldo());
+	}
+}
+
